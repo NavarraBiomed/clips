83b94dd912769e5b06add17f213645370ca69ba3
Tomisu <tomastarragon@gmail.com>
2016-04-19 10:26:52 +0200

    Added logout

--- clips_app/urls.py
+++ clips_app/urls.py
@@ -11,8 +11,8 @@ urlpatterns = [
 	url (r'^validate/$', views.validate_score),
 	url (r'^study/(?P<study_id>[0-9]+)/json/$', views.study_json),
 	url (r'^study/(?P<study_id>[0-9]+)/info/$', views.study_info),
-	url(r'^case/(?P<pk>[0-9]+)/edit/$', views.case_edit, name ='case_edit'),
-
+	url (r'^case/(?P<pk>[0-9]+)/edit/$', views.case_edit, name ='case_edit'),
+	url (r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
     #url(r'^admin/', RedirectView.as_view(url='/admin'))
     #url(r'^/new/$', views.form_test)
 
